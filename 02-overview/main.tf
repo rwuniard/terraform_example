1595420fbee1bee3621d9dae98d1f075fc2f7fda
Ronson Wuniardy <rwuniard@hotmail.com>
2022-07-11 11:26:35 -0400

    Adding vpc config

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -14,4 +14,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-01a415f35b905d604"]
+  subnet_id = "subnet-037547744291ec68d"
 }
