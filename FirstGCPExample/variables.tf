61b2bce54fbe920e00a39e8c7966096317383e81
Ronson Wuniardy <rwuniard@hotmail.com>
2022-07-12 19:25:31 -0400

    Added GKE

---
+++ FirstGCPExample/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "terraform-356015"
+}
+
+variable "credentials_file" {
+    default = "secret.json"
+}
+
+variable "region" {
+    default = "us-central1"
+}
+
+variable "zone" {
+    default = "us-central1-c"
+}
---
+++ GKE-cluster/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "terraform-356015"
+region     = "us-central1"
